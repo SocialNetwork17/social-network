b34d469c8c6ab6b7d10e1b67ba45e82d6f82edc3
MariaSemenovaDev <semenovamk96@mail.ru>
2025-11-10 09:36:38 +0300

    build: add storybook addon

---
+++ src/app/(admin)/usersList/page.tsx
@@ -0,0 +1,9 @@
+import UserList from "@/app/(admin)/usersList/_components/page";
+
+export default function UserListPage() {
+    return (
+        <>
+            <UserList/>
+        </>
+    );
+}
