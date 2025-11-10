f0a04734cd608a9fa8628cf37bbe726777cdf6bc
LuckyF1re <andruha.sadovich@gmail.com>
2025-11-10 06:39:32 +0300

    SF-20 refactor: refactor Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import {Header} from "@/widgets/header/ui/Header";
 
 export default function Home() {
         return (
     <div className={styles.page}>
       <main className={styles.main}>
-         <Header></Header>
         <Image
           className={styles.logo}
           src="/next.svg"
