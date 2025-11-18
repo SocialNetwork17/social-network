f0a227a5d93ab7d10720a17f5b251cb8e233336d
annaSemenova <anna.7nova@yandex.by>
2025-11-18 14:14:41 +0300

    feat(MainPage): add styles

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -3,15 +3,22 @@ import styles from './MainPage.module.scss'
 import CardWithText from '@/shared/ui/CardWithText/CardWithText'
 
 export default function MainPage() {
+  const totalRegisteredUser = '009213' //приходят данные с backend
+  const arrayTotalUser = totalRegisteredUser.split('')
+  
   return (
     <div className={styles.container}>
       <div>
         <div>Registered users:</div>
-        <div>009213</div>
+        <div>
+          {arrayTotalUser.map((el, index) => (
+            <span key={index}>{el}</span>
+          ))}
+        </div>
       </div>
       <div>
         {postsData.map(el => (
-          <CardWithText user={el} key={el.id}/>
+          <CardWithText user={el} key={el.id} />
         ))}
       </div>
     </div>
