4789e2dd3866665fda5e289ce42eeb9063e5a318
MariaSemenovaDev <semenovamk96@mail.ru>
2025-12-01 21:42:58 +0300

    feat(signIn): add signIn page

--- src/features/signIn/ui/SignInForm.tsx
+++ src/features/signIn/ui/SignInForm.tsx
@@ -1,22 +1,44 @@
+'use client'
+
 import styles from "./SignInForm.module.scss";
 import {SignInFormTitle} from "@/features/signIn/ui/singInFormTitle/SingInFormTitle";
 import {Input} from "@/shared/ui/Input/Input";
 import Link from "next/link";
 import {PATH} from "@/shared/constants/routings";
 import {Button} from "@/shared/ui/Button/Button";
+import {useState} from "react";
 
 export const SignInForm = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    // const loginMutation = useLoginMutation();
+
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        console.log("Email:", email);
+        console.log("Password:", password);
+    };
+
+
     return (
         <div className={styles.authCard}>
             <SignInFormTitle/>
 
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <div className={styles.inputWrapper}>
                     <Input
                         label={"Email"}
                         type={"email"}
                         placeholder={"epam@epam.com"}
                         required
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        // disabled={loginMutation.isPending}
                     />
 
                     <Input
@@ -24,13 +46,17 @@ export const SignInForm = () => {
                         type={"password"}
                         placeholder={"add password"}
                         required
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        // disabled={loginMutation.isPending}
                     />
                 </div>
 
                 <div>
                     <Link href={PATH.FORGOT_PASSWORD} className={styles.fargotPasswordLink}>Forgot Password</Link>
 
-                    <Button variant={"primary"} disabled={false}>
+                    <Button variant={"primary"} disabled={false}
+                            type="submit">
                         Sign In
                     </Button>
 
