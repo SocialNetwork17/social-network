4dec0660bcf7d5e0dacb32c7fb463f25336401da
Alexey Muhametshin - AmaLOQ <muhametshin.alexey@gmail.com>
2026-01-28 19:35:37 +0700

    fix(*): fix

--- src/shared/ui/Skeleton/Skeleton.stories.tsx
+++ src/shared/ui/Skeleton/Skeleton.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/nextjs-vite'
-import Skeleton from './Skeleton'
+import {Skeleton} from './Skeleton'
 
 const meta = {
   title: 'shared/ui/Skeleton',
