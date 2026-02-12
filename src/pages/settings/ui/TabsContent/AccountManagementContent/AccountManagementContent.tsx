ffeeeed48b37ab12ad7c179797d287a6e44ead7f
Evhen87 <kushner.aleks@yandex.ru>
2026-02-13 00:29:16 +0300

    init: implemented the layout of the Account Management page and Radio Button

--- src/pages/settings/ui/TabsContent/AccountManagementContent/AccountManagementContent.tsx
+++ src/pages/settings/ui/TabsContent/AccountManagementContent/AccountManagementContent.tsx
@@ -1,14 +1,57 @@
 import styles from "./AccountManagementContent.module.scss"
-import {EditProfileForm} from "@/features/editProfile/ui/EditProfileForm";
 
-import {EditAvatar} from "@/features/editAvatar/ui/EditAvatar";
+import {RadioGroup} from "@/shared/ui/Radio/RadioGroup";
+import {useState} from "react";
+import {IconButton} from "@/shared/ui/IconButton/IconButton";
 
 export const AccountManagementContent = () => {
+    const [accountType, setAccountType] = useState('personal')
+    const [costsValue, setCostsValue] = useState('oneDay')
 
     return (
         <div className={styles.accountManagementContainer}>
-            <EditAvatar/>
-            <EditProfileForm/>
+            <div className={styles.accountManagementBlock}>
+                <h4 className={styles.accountManagementTitle}>Account type:</h4>
+                <div className={styles.radioGroupContainer}>
+                    <RadioGroup
+                        name="tariff"
+                        options={[
+                            { value: 'personal', label: 'Personal'},
+                            { value: 'business', label: 'Business' },
+                        ]}
+                        value={accountType}
+                        onChange={setAccountType}
+                    />
+                </div>
+            </div>
+            {accountType ===  'business' &&
+            <div className={styles.accountManagementBlock}>
+                <h4 className={styles.accountManagementTitle}>Your subscription costs:</h4>
+                <div className={styles.radioGroupContainer}>
+                    <RadioGroup
+                        name="costs"
+                        options={[
+                            { value: 'oneDay', label: '$10 per 1 Day'},
+                            { value: 'sevenDay', label: '$50 per 7 Day' },
+                            { value: 'month', label: '$100 per month'},
+                        ]}
+                        value={costsValue}
+                        onChange={setCostsValue}
+                    />
+                </div>
+                <div className={styles.paymentBlock}>
+                    <IconButton iconId={'paypal'}
+                                size={96}
+                                viewBox={'0 0 96 64'}
+                    />
+                    <span className={styles.paymentBlock}>or</span>
+                    <IconButton iconId={'stripe'}
+                                size={96}
+                                viewBox={'0 0 96 64'}
+                    />
+                </div>
+            </div>
+            }
         </div>
     )
 }
