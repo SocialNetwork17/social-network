14efd9b60f6c0e2febdd22b394475054c6e22278
Dmitry Shimansky <shimansky.dmitry@gmail.com>
2026-03-13 18:14:40 +0300

    Fixed login

--- src/features/signIn/ui/SignInForm.tsx
+++ src/features/signIn/ui/SignInForm.tsx
@@ -11,8 +11,11 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { SignInFormValues, signInSchema } from '@/features/signIn/lib/signInSchema'
 import { useLoginMutation } from '@/features/signIn/api/useLoginMutation'
 import { Spinner } from '@/shared/ui/Spinner/Spinner'
+import { useRouter } from 'next/navigation'
 
 export const SignInForm = () => {
+  const router = useRouter()
+
   const {
     register,
     handleSubmit,
@@ -26,6 +29,7 @@ export const SignInForm = () => {
 
   const onSubmit = (data: SignInFormValues) => {
     loginMutation({ ...data, email: data.email.toLowerCase() })
+    router.replace(PATH.PROFILE)
   }
 
   return (
