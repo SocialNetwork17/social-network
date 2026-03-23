7383c4d199d5ffc2b480f9ec63c3a62df1f65880
AmaLOQ <muhametshin.alexey@gmail.com>
2026-03-23 15:03:57 +0700

    feat(WebSocket): WebSocket first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{ children: React.ReactNode }>) {
 
     return (
-        <html lang="en"
-              //className={'light'}
-        >
+        <html lang="en">
         <body>
         <QueryProvider>
             <RootLayoutClient>
