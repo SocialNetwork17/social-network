4c55e9f6740d9468d81dfbbb7cdcf99c377f8217
AmaLOQ <muhametshin.alexey@gmail.com>
2026-03-24 17:42:32 +0700

    fix(ThemeSwitcher): fix bugs theme switcher

--- src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -6,19 +6,6 @@ export const ThemeSwitcher = () => {
     const [theme, setTheme] = useState<'light' | 'dark'>('dark');
 
     useEffect(() => {
-        // Initialize from system preference
-        const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-        const initialTheme = prefersDark ? 'dark' : 'light';
-        setTheme(initialTheme);
-    }, []);
-
-    useEffect(() => {
-        /**
-         * When theme === 'light' is true:
-         * Result: Adds 'light' class (if not already present)
-         * Keeps 'light' class if already present
-         * Equivalent to: document.documentElement.classList.add('light')
-         */
         document.documentElement.classList.toggle('light', theme === 'light');
     }, [theme]);
 
@@ -29,11 +16,11 @@ export const ThemeSwitcher = () => {
     return (
         <>
             {theme === 'light' ? (
-                <Button variant={'textButton'} disabled={false} width={36} height={36} onClickHandler={toggleTheme} >
+                <Button variant={'textButton'} disabled={false} width={36} height={36} onClick={toggleTheme} >
                     <Icon iconId={'moon'} size={20} stroke={'black'}/>
                 </Button>
             ) : (
-                <Button variant={'textButton'} disabled={false} width={36} height={36} onClickHandler={toggleTheme} >
+                <Button variant={'textButton'} disabled={false} width={36} height={36} onClick={toggleTheme} >
                     <Icon iconId={'sun'} size={20} stroke={'white'}/>
                 </Button>
             )}
